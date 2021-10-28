98de51b1ad1c0cd725930dca24048ac237f24693
studentDG <daegu776@gmail.com>
2021-10-28 21:20:10 +0900

    Initial commit

---
+++ app/src/main/java/com/example/test2/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.test2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val mail = "daegu776@gmail.com"
+
+        val pw = "qwer"
+
+        loginBtn.setOnClickListener {
+            if(mail && pw)
+
+        }
+
+
+
+
+
+
+    }
+}
